55b61b27a53b3568db627830b267adee333adef5
Nathaniel Yao <nathanhyao@gmail.com>
2020-01-17 11:44:07 -0500

    Hello 1-17

---
+++ MyFirstRobot/src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -0,0 +1,49 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.RobotMap;
+import frc.robot.commands.TankDrive;
+
+/**
+ * Add your docs here.
+ */
+public class DriveTrain extends Subsystem {
+  //PART 2 12:06
+  private TalonSRX motorLeft1 = new TalonSRX(RobotMap.MOTOR_LEFT_1_ID);
+  private TalonSRX motorLeft2 = new TalonSRX(RobotMap.MOTOR_LEFT_2_ID);
+  private TalonSRX motorRight1 = new TalonSRX(RobotMap.MOTOR_RIGHT_1_ID);
+  private TalonSRX motorRight2 = new TalonSRX(RobotMap.MOTOR_RIGHT_2_ID);
+  // PRIVATE Because somewhere else in the code may damage the motors.
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+    setDefaultCommand(new TankDrive());//VIDEO AT 1-17-2020 Part 2 32:27
+    //TO DEPLOY THE CODE TO A ROBOT: CTRL+SHIFT+P > DEPLOY ROBOT CODE OR BUILD (TO DEMO)
+    //CONNECTED; COMMUNICATIONS LIGHT IS GREEN
+    //WHEN ROBOT CODE IS GREEN, GO AHEAD AND ENABLE AND DRIVE
+  }
+  //public, void = don't care about retunring anything; 
+  public void setLeftMotors(double speed){
+    motorLeft1.set(ControlMode.PercentOutput, -speed);
+    motorLeft2.set(ControlMode.PercentOutput, -speed);
+  }
+  //setR/L Motors taking double value and giving that info to the Motor Controllers.
+  public void setRightMotors(double speed){
+    motorRight1.set(ControlMode.PercentOutput, speed);
+    motorRight2.set(ControlMode.PercentOutput, speed);
+  }
+}
